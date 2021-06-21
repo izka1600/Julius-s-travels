b2b8a5781e00f570b2821e0f2d28f49647708457
Iza Kukawska <izakukawska1600@gmail.com>
2021-06-21 18:31:16 +0200

    first css changes add map to website

--- WebJourneys/WebJourneys/wwwroot/js/site.js
+++ WebJourneys/WebJourneys/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var map = L.map('mapid').setView([52.230, 21.00], 10);
+
+// add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
+
+// show the scale bar on the lower left corner
+L.control.scale().addTo(map);
+
+// show a marker on the map
+L.marker({ lon: 21.00, lat: 52.230 }).bindPopup('The start of the journey').addTo(map);
